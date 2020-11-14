1a78e27a6ca386f25ff08436d7d2e905aea21ce5
varioustoxins <g.s.thompson@kent.ac.uk>
2020-11-14 12:17:31 +0000

    add support for sha256 and sha512 in yaml files tidy expand flag handling

--- lib/yaml.py
+++ lib/yaml.py
@@ -16,17 +16,21 @@ def read_releases(package,version, resource):
     for release_id, release in releases.items():
         url = f"{release['root_url']}/{release['install_file']}"
         version_number = release['version']
-        check =  release['md5']
+
+        for check_sum_name in 'md5', 'sha256', 'sha512':
+            if check_sum_name in release:
+                check_sum = release[check_sum_name]
 
         if 'url_type' in release:
             url_arg = release['url_type']
         else:
             url_arg = 'url'
-        url_param = {url_arg : url}
+        params = {url_arg : url,  check_sum_name : check_sum}
+
         expand=False
         if 'expand' in release:
             expand= release['expand']
-        version(version_number, **url_param, md5=check, expand=expand)
+        version(version_number, **params, expand=expand)
 
         # NOTE: we could be downloading resources we don't need because a variant is marked as not required
         # however, how to access variants from the spec at this point...
