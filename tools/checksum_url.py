b24d5958c261034a0b2db8495343e6166a80e7ef
varioustoxins <g.s.thompson@kent.ac.uk>
2020-11-23 22:50:03 +0000

    add support for scanning web page for files matching templates

--- tools/checksum_url.py
+++ tools/checksum_url.py
@@ -7,6 +7,8 @@ import sys
 from urllib.parse import urlparse
 from os.path import split
 from argparse import RawTextHelpFormatter
+from bs4 import BeautifulSoup,SoupStrainer
+from fnmatch import fnmatch
 
 def test_hash_length():
     m = hashlib.md5()
@@ -50,9 +52,8 @@ class DownloadFailedException(Exception):
         super(DownloadFailedException, self).__init__(msg)
 
 
-def get_hash_from_url(url, show_progress=False, root=None, digest='sha256'):
-    if root:
-        url = f'{root}/{url}'
+def get_hash_from_url(url, show_progress=False,  digest='sha256'):
+
 
     response = requests.get(url, allow_redirects=True, timeout=10, stream=True)
     total_data_length = response.headers.get('content-length')
@@ -83,7 +84,7 @@ def get_hash_from_url(url, show_progress=False, root=None, digest='sha256'):
     return digester.hexdigest()
 
 
-def report_error():
+def report_error(e):
     msg = " ".join(e.args)
     sys.stdout.write(f"\r{url} {msg}")
 
@@ -111,23 +112,77 @@ def digests_formatted():
     digests = '\n'.join(digests)
     return digests
 
+
+def check_root_set_or_exit(args):
+    if not args.root:
+        print('argument --template requires matching root_argument')
+        print('exiting...')
+        sys.exit()
+
+
+def get_urls_for_templates(root, templates):
+    urls = []
+
+    response = requests.get(root)
+
+    for link in BeautifulSoup(response.content, parse_only=SoupStrainer('a'), features="html.parser"):
+        if link.has_attr('href'):
+            urls.append(link['href'])
+
+    result = set()
+    for url in urls:
+        for template in templates:
+            if fnmatch(url.rsplit('/', 1)[-1], template):
+                result.add(url)
+                
+    return list(result)
+
+
+def get_urls_from_args(args):
+
+    if args.use_templates:
+        urls = get_urls_for_templates(args.root, args.urls)
+    elif args.root:
+        urls = [f'{args.root}/{url}' for url in args.urls]
+    else:
+        urls = args.urls
+    
+    return urls
+
+
+def check_root_exists_or_exit(root):
+
+    request = requests.get(root)
+    if request.status_code != 200:
+        print(f"the web site {root} isn't acessible")
+        print('exiting...')
+        sys.exit()
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='calculate hashes from web links.', formatter_class=RawTextHelpFormatter)
     parser.add_argument('-v','--verbose', dest='verbose', default=False, action='store_true', help='verbose output with origress bars')
     parser.add_argument('-e','--fail-early', dest='fail_early', default=False, action='store_true', help='exit on first error')
-    parser.add_argument('-r', '--root', dest='root', help='root url to add command line arguments to')
+    parser.add_argument('-r', '--root', dest='root', default=None, help='root url to add command line arguments to')
     parser.add_argument('-d', '--digest', dest ='digest', default='sha256', help=f'which digest algorithm to use: \n\n{digests_formatted()}\n')
+    parser.add_argument('-t', '--template', dest='use_templates', default=False, action='store_true',
+                        help='if set all filenames are treated as templates in the root page, --root must also be set...')
     parser.add_argument('urls', nargs='+')
     args = parser.parse_args()
 
-    for url in args.urls:
+    if args.root:
+        check_root_exists_or_exit(args.root)
+
+    urls = get_urls_from_args(args)
+
+    for url in urls:
         try:
-            hash = get_hash_from_url(url, args.verbose, root=args.root,digest=args.digest)
+            hash = get_hash_from_url(url, args.verbose, digest=args.digest)
             display_hash(url, hash)
         except DownloadFailedException as e:
 
-            report_error()
+            report_error(e)
 
             if args.fail_early:
                 exit_if_asked()
