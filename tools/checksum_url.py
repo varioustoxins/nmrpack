aed954d25c425681b399f41774a01d50c2c2d75b
varioustoxins <garyt.and.sarahb@gmail.com>
2020-11-28 23:21:06 +0000

    add the ability to login to a site via a web form and session

--- tools/checksum_url.py
+++ tools/checksum_url.py
@@ -10,6 +10,7 @@ from argparse import RawTextHelpFormatter
 from bs4 import BeautifulSoup,SoupStrainer
 from fnmatch import fnmatch
 
+session = None
 def test_hash_length():
     m = hashlib.md5()
     m.update(b'wibble')
@@ -48,10 +49,21 @@ class DownloadFailedException(Exception):
         super(DownloadFailedException, self).__init__(msg)
 
 
-def get_hash_from_url(url, show_progress=False,  digest='sha256'):
+def get_hash_from_url(url, session, show_progress=False,  digest='sha256', username_password=(None,None)):
 
+    username, password = username_password
+    if username_password != (None,None):
 
-    response = requests.get(url, allow_redirects=True, timeout=10, stream=True)
+        auth = requests.auth.HTTPBasicAuth(username, password)
+        response = session.get(url, allow_redirects=True, timeout=10, stream=True, auth=auth)
+
+        if response.status_code != 200:
+            auth = requests.auth.HTTPDigestAuth(username,password)
+            response = session.get(url, allow_redirects=True, timeout=10, stream=True, auth=auth)
+
+    else:
+        response = session.get(url, allow_redirects=True, timeout=10, stream=True)
+
     total_data_length = response.headers.get('content-length')
 
     digester = getattr(hashlib, digest)()
@@ -114,44 +126,69 @@ def check_root_set_or_exit(args):
         sys.exit()
 
 
-def get_urls_for_templates(root, templates):
+def get_urls_for_templates(page, root, templates):
+
     urls = []
+    result = []
+    for link in BeautifulSoup(page, parse_only=SoupStrainer('a') features="html.parser"):
 
-    response = requests.get(root)
-
-    for link in BeautifulSoup(response.content, parse_only=SoupStrainer('a'), features="html.parser"):
         if link.has_attr('href'):
-            urls.append(link['href'])
+                urls.append(link['href'])
 
-    result = set()
-    for url in urls:
-        for template in templates:
-            if fnmatch(url.rsplit('/', 1)[-1], template):
-                result.add(url)
-                
+        result = set()
+        for url in urls:
+            for template in templates:
+                if fnmatch(url.rsplit('/', 1)[-1], template):
+                    result.add(url)
+
     return list(result)
 
 
-def get_urls_from_args(args):
+def get_urls_from_args(root, urls):
 
-    if args.use_templates:
-        urls = get_urls_for_templates(args.root, args.urls)
-    elif args.root:
+    if root:
         urls = [f'{args.root}/{url}' for url in args.urls]
-    else:
-        urls = args.urls
     
     return urls
 
 
-def check_root_exists_or_exit(root):
+def check_root_exists_or_exit(root, sesssion, username_password=(None,None)):
 
-    request = requests.get(root)
-    if request.status_code != 200:
+    response = download_page(root, session, username_password)
+
+    if response.status_code != 200:
         print(f"the web site {root} isn't acessible")
         print('exiting...')
         sys.exit()
 
+
+def download_page(root, session, username_password, form=None):
+    username, password = username_password
+    if username_password != (None, None) and not form:
+
+        auth = requests.auth.HTTPBasicAuth(username, password)
+        response = session.get(root, allow_redirects=True, timeout=10, stream=True, auth=auth)
+
+        if response.status_code != 200:
+            auth = requests.auth.HTTPDigestAuth(username, password)
+            response = session.get(root, allow_redirects=True, timeout=10, stream=True, auth=auth)
+    else:
+        response = session.get(root, allow_redirects=True, timeout=10, stream=True)
+    return response.content
+
+
+def login_with_form(session, username_password, form):
+    username, password = username_password
+    user_field, pass_field, submit_field = form
+    session = requests.Session()
+    # for xplor these are 'UserName, Password, .submit'
+    data = {user_field: username, pass_field: password, submit_field: 'y'}
+
+    response = session.post(args.root, data=data)
+    if response.status_code != 200:
+        raise DownloadFailedException(f"couldn't open the password page\n\n{response.text}")
+
+    return response.content
 
 if __name__ == '__main__':
 
@@ -161,18 +198,36 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--root', dest='root', default=None, help='root url to add command line arguments to')
     parser.add_argument('-d', '--digest', dest ='digest', default='sha256', help=f'which digest algorithm to use: \n\n{digests_formatted()}\n')
     parser.add_argument('-t', '--template', dest='use_templates', default=False, action='store_true',
-                        help='if set all filenames are treated as templates in the root page, --root must also be set...')
-    parser.add_argument('urls', nargs='+')
+                        help='use urls as unix filename templates and scan the root page, --root must also be set...')
+    parser.add_argument('-p', '--password', dest='password', nargs=2,  default=(None,None), help='provide a username and password', metavar=('USERNAME','PASSWORD'))
+    parser.add_argument('-f','--form', default=None, dest='form', nargs=3, help='use a form for login use root for the url of the form', metavar=('USERNAME_FIELD','PASSWORD_FIELD', 'SUBMIT_FIELD'))
+    parser.add_argument('urls', nargs='*')
     args = parser.parse_args()
 
-    if args.root:
-        check_root_exists_or_exit(args.root)
+    args.password = tuple(args.password)
 
-    urls = get_urls_from_args(args)
+    session = requests.session()
+
+    page = None
+    if args.form:
+        page = login_with_form(session, args.password, args.form)
+    else:
+        check_root_exists_or_exit(args.root, session, username_password=args.password)
+
+    if args.form and args.root and not args.use_templates:
+        urls = args.urls
+    elif args.root and not args.use_templates:
+        urls = get_urls_from_args(args.root, args.urls)
+    elif args.use_templates:
+        if not page:
+            page = download_page(args.root, session, args.password)
+        urls = get_urls_for_templates(page, args.root, args.urls)
+    else:
+        print(f'Bad combination of template {args.use_templates} and root {args.root}')
 
     for url in urls:
         try:
-            hash = get_hash_from_url(url, args.verbose, digest=args.digest)
+            hash = get_hash_from_url(url, session, args.verbose, digest=args.digest, username_password=args.password)
             display_hash(url, hash)
         except DownloadFailedException as e:
 
