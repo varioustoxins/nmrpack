3e28f37df55a884bc92265d0f88b394e2e2b1602
varioustoxins <garyt.and.sarahb@gmail.com>
2020-11-29 00:05:57 +0000

    clean up

--- tools/checksum_url.py
+++ tools/checksum_url.py
@@ -7,27 +7,31 @@ import sys
 from urllib.parse import urlparse
 from os.path import split
 from argparse import RawTextHelpFormatter
-from bs4 import BeautifulSoup,SoupStrainer
+from bs4 import BeautifulSoup, SoupStrainer
 from fnmatch import fnmatch
 
 session = None
+
+
 def test_hash_length():
     m = hashlib.md5()
-    m.update(b'wibble')
+    m.update(b'flibbertigibbet')
     return len(m.hexdigest())
 
+
 def exception_to_message(exception):
-    identifier =  exception.__class__.__name__
+    identifier = exception.__class__.__name__
     matches = finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
     camels = [match.group(0).lower() for match in matches]
     return ' '.join(camels)
 
-def write_digest(url, digest):
-    sys.stdout.write(f"\r{url} {digest}")
+
+def write_digest(target_url, digest):
+    sys.stdout.write(f"\r{target_url} {digest}")
 
 
-def get_failure_message(url, message):
-    return f"\r{url} download failed [{message}]"
+def get_failure_message(target_url, message):
+    return f"\r{target_url} download failed [{message}]"
 
 
 def get_bar(display_length, data_length, total_data_length):
@@ -35,10 +39,10 @@ def get_bar(display_length, data_length, total_data_length):
     return '[%s%s]' % ('=' * done, ' ' * (display_length - done))
 
 
-def write_progress(url, display_length, data_length, total_data_length):
+def write_progress(target_url, display_length, data_length, total_data_length):
 
     bar = get_bar(display_length, data_length, total_data_length)
-    url_components = urlparse(url)
+    url_components = urlparse(target_url)
     file = split(url_components.path)[-1]
     sys.stdout.write(f"\r{file} {bar}")
     sys.stdout.flush()
@@ -49,20 +53,9 @@ class DownloadFailedException(Exception):
         super(DownloadFailedException, self).__init__(msg)
 
 
-def get_hash_from_url(url, session, show_progress=False,  digest='sha256', username_password=(None,None)):
+def get_hash_from_url(target_url, target_session, show_progress=False, digest='sha256', username_password=(None, None)):
 
-    username, password = username_password
-    if username_password != (None,None):
-
-        auth = requests.auth.HTTPBasicAuth(username, password)
-        response = session.get(url, allow_redirects=True, timeout=10, stream=True, auth=auth)
-
-        if response.status_code != 200:
-            auth = requests.auth.HTTPDigestAuth(username,password)
-            response = session.get(url, allow_redirects=True, timeout=10, stream=True, auth=auth)
-
-    else:
-        response = session.get(url, allow_redirects=True, timeout=10, stream=True)
+    response = transfer_page(target_session, url, username_password)
 
     total_data_length = response.headers.get('content-length')
 
@@ -83,15 +76,31 @@ def get_hash_from_url(url, session, show_progress=False,  digest='sha256', usern
                 digester.update(data)
 
                 if show_progress:
-                    write_progress(url, display_length, data_length, total_data_length)
+                    write_progress(target_url, display_length, data_length, total_data_length)
 
-        except Exception as e:
-            raise DownloadFailedException(get_failure_message(exception_to_message(e)))
+        except Exception as exception:
+            raise DownloadFailedException(get_failure_message(target_url, exception_to_message(exception)))
     return digester.hexdigest()
 
 
-def report_error(e):
-    msg = " ".join(e.args)
+def transfer_page(target_session, target_url, username_password):
+    if username_password != (None, None):
+        username, password = username_password
+
+        auth = requests.auth.HTTPBasicAuth(username, password)
+        response = target_session.get(target_url, allow_redirects=True, timeout=10, stream=True, auth=auth)
+
+        if response.status_code != 200:
+            auth = requests.auth.HTTPDigestAuth(username, password)
+            response = target_session.get(target_url, allow_redirects=True, timeout=10, stream=True, auth=auth)
+
+    else:
+        response = target_session.get(target_url, allow_redirects=True, timeout=10, stream=True)
+    return response
+
+
+def report_error(error):
+    msg = " ".join(error.args)
     sys.stdout.write(f"\r{url} {msg}")
 
 
@@ -102,105 +111,101 @@ def exit_if_asked():
     sys.exit(1)
 
 
-def display_hash(url, hash):
-    sys.stdout.write(f"\r{url} {hash}")
+def display_hash(target_url, _hash):
+    sys.stdout.write(f"\r{target_url} {_hash}")
+
 
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
-NEW_LINE='\n'
+
+NEW_LINE = '\n'
+
 
 def digests_formatted():
-    digests = list(chunks(list(hashlib.algorithms_available),5))
+    digests = list(chunks(list(hashlib.algorithms_available), 5))
     digests = [', '.join(group) for group in digests]
     digests = '\n'.join(digests)
     return digests
 
 
-def check_root_set_or_exit(args):
-    if not args.root:
+def check_root_set_or_exit(in_args):
+    if not in_args.root:
         print('argument --template requires matching root_argument')
         print('exiting...')
         sys.exit()
 
 
-def get_urls_for_templates(page, root, templates):
+def get_urls_for_templates(in_page, templates):
 
-    urls = []
+    target_urls = []
     result = []
-    for link in BeautifulSoup(page, parse_only=SoupStrainer('a') features="html.parser"):
+    for link in BeautifulSoup(in_page, parse_only=SoupStrainer('a'), features="html.parser"):
 
         if link.has_attr('href'):
-                urls.append(link['href'])
+            target_urls.append(link['href'])
 
         result = set()
-        for url in urls:
+        for target_url in target_urls:
             for template in templates:
-                if fnmatch(url.rsplit('/', 1)[-1], template):
-                    result.add(url)
+                if fnmatch(target_url.rsplit('/', 1)[-1], template):
+                    result.add(target_url)
 
     return list(result)
 
 
-def get_urls_from_args(root, urls):
+def get_urls_from_args(root, target_urls):
 
     if root:
-        urls = [f'{args.root}/{url}' for url in args.urls]
+        target_urls = [f'{args.root}/{target_url}' for target_url in target_urls]
     
-    return urls
+    return target_urls
 
 
-def check_root_exists_or_exit(root, sesssion, username_password=(None,None)):
+def check_root_exists_or_exit(root, target_session, username_password=(None, None)):
 
-    response = download_page(root, session, username_password)
+    response = transfer_page(target_session, root, username_password)
 
     if response.status_code != 200:
-        print(f"the web site {root} isn't acessible")
+        print(f"the web site {root} isn't accessible")
         print('exiting...')
         sys.exit()
 
 
-def download_page(root, session, username_password, form=None):
-    username, password = username_password
-    if username_password != (None, None) and not form:
-
-        auth = requests.auth.HTTPBasicAuth(username, password)
-        response = session.get(root, allow_redirects=True, timeout=10, stream=True, auth=auth)
-
-        if response.status_code != 200:
-            auth = requests.auth.HTTPDigestAuth(username, password)
-            response = session.get(root, allow_redirects=True, timeout=10, stream=True, auth=auth)
-    else:
-        response = session.get(root, allow_redirects=True, timeout=10, stream=True)
-    return response.content
-
-
-def login_with_form(session, username_password, form):
+def login_with_form(target_session, username_password, form):
     username, password = username_password
     user_field, pass_field, submit_field = form
-    session = requests.Session()
+
     # for xplor these are 'UserName, Password, .submit'
     data = {user_field: username, pass_field: password, submit_field: 'y'}
 
-    response = session.post(args.root, data=data)
+    response = target_session.post(args.root, data=data)
     if response.status_code != 200:
         raise DownloadFailedException(f"couldn't open the password page\n\n{response.text}")
 
     return response.content
 
+
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='calculate hashes from web links.', formatter_class=RawTextHelpFormatter)
-    parser.add_argument('-v','--verbose', dest='verbose', default=False, action='store_true', help='verbose output with origress bars')
-    parser.add_argument('-e','--fail-early', dest='fail_early', default=False, action='store_true', help='exit on first error')
+    parser = argparse.ArgumentParser(description='calculate hashes from web links.',
+                                     formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
+                        help='verbose output with progress bars')
+    parser.add_argument('-e', '--fail-early', dest='fail_early', default=False, action='store_true',
+                        help='exit on first error')
     parser.add_argument('-r', '--root', dest='root', default=None, help='root url to add command line arguments to')
-    parser.add_argument('-d', '--digest', dest ='digest', default='sha256', help=f'which digest algorithm to use: \n\n{digests_formatted()}\n')
+    parser.add_argument('-d', '--digest', dest='digest', default='sha256',
+                        help=f'which digest algorithm to use: \n\n{digests_formatted()}\n')
     parser.add_argument('-t', '--template', dest='use_templates', default=False, action='store_true',
                         help='use urls as unix filename templates and scan the root page, --root must also be set...')
-    parser.add_argument('-p', '--password', dest='password', nargs=2,  default=(None,None), help='provide a username and password', metavar=('USERNAME','PASSWORD'))
-    parser.add_argument('-f','--form', default=None, dest='form', nargs=3, help='use a form for login use root for the url of the form', metavar=('USERNAME_FIELD','PASSWORD_FIELD', 'SUBMIT_FIELD'))
+    parser.add_argument('-p', '--password', dest='password', nargs=2,  default=(None, None),
+                        help='provide a username and password', metavar=('USERNAME', 'PASSWORD'))
+    parser.add_argument('-f', '--form', default=None, dest='form', nargs=3,
+                        help='use a form for login use root for the url of the form',
+                        metavar=('USERNAME_FIELD', 'PASSWORD_FIELD', 'SUBMIT_FIELD'))
     parser.add_argument('urls', nargs='*')
     args = parser.parse_args()
 
@@ -209,6 +214,7 @@ if __name__ == '__main__':
     session = requests.session()
 
     page = None
+    urls = []
     if args.form:
         page = login_with_form(session, args.password, args.form)
     else:
@@ -220,15 +226,15 @@ if __name__ == '__main__':
         urls = get_urls_from_args(args.root, args.urls)
     elif args.use_templates:
         if not page:
-            page = download_page(args.root, session, args.password)
-        urls = get_urls_for_templates(page, args.root, args.urls)
+            page = transfer_page(args.root, session, args.password)
+        urls = get_urls_for_templates(page,  args.urls)
     else:
         print(f'Bad combination of template {args.use_templates} and root {args.root}')
 
     for url in urls:
         try:
-            hash = get_hash_from_url(url, session, args.verbose, digest=args.digest, username_password=args.password)
-            display_hash(url, hash)
+            _hash = get_hash_from_url(url, session, args.verbose, digest=args.digest, username_password=args.password)
+            display_hash(url, _hash)
         except DownloadFailedException as e:
 
             report_error(e)
@@ -236,4 +242,3 @@ if __name__ == '__main__':
             if args.fail_early:
                 exit_if_asked()
         print()
-
