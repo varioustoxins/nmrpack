954b8b0c7ab5afbc5a50c8e4702ee090d8eab192
varioustoxins <garyt.and.sarahb@gmail.com>
2020-12-27 18:21:08 +0000

    translate the pip filenames to use the shorter pip hidden interface url

--- tools/checksum_plugins/pip_navigator_plugin.py
+++ tools/checksum_plugins/pip_navigator_plugin.py
@@ -12,20 +12,32 @@ class PipNavigator(UrlNavigator):
         super(PipNavigator, self).__init__(browser, target_args)
 
     @classmethod
-    def _update_dict(cls, targets, sources):
-        return target
+    def _translate_pip_urls(cls, urls):
+        results = []
+        for url in urls:
+            file_name = url.split('/')[-1]
+            first_letter = file_name[0]
+            package_name = file_name.split('-')[0]
+
+            new_url = f'https://pypi.io/packages/source/{first_letter}/{package_name}/{file_name}'
+
+            results.append(new_url)
+
+        return results
+
 
     def get_urls(self, sorted_by_version=True):
 
-        result = super(PipNavigator, self).get_urls()
+        results = super(PipNavigator, self).get_urls()
 
         if sorted_by_version:
-            url_versions = [arg.split('#')[0] for arg in result]
+            url_versions = [arg.split('#')[0] for arg in results]
             url_versions = self._urls_to_url_version(url_versions, self._args.version_regex)
             url_versions = self._sort_url_versions(url_versions)
-            result = list(url_versions.keys())
+            results = list(url_versions.keys())
 
-        return result
+        results = self._translate_pip_urls(results)
+        return results
 
 
 class PipNavigatorFactory:
