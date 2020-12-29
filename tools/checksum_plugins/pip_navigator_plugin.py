de7c45d47e05b87361de5870b01dcd8407f8a452
varioustoxins <garyt.and.sarahb@gmail.com>
2020-12-29 18:01:06 +0000

    pip navigator now uses the new plugin registration system

--- tools/checksum_plugins/pip_navigator_plugin.py
+++ tools/checksum_plugins/pip_navigator_plugin.py
@@ -1,12 +1,15 @@
 import pluggy
 # noinspection PyUnresolvedReferences
-from checksum_url import CHECK_SUM_PROJECT, CHECK_SUM_IMPL, Navigator, transfer_page
+from checksum_url import Navigator, transfer_page
+from plugins import register_navigator
 from .url_navigator_plugin import UrlNavigator
 
-pm = pluggy.PluginManager(CHECK_SUM_PROJECT)
 
 
+@register_navigator()
 class PipNavigator(UrlNavigator):
+
+    NAME = 'pip'
 
     def __init__(self, browser, target_args):
         super(PipNavigator, self).__init__(browser, target_args)
@@ -25,7 +28,6 @@ class PipNavigator(UrlNavigator):
 
         return results
 
-
     def get_urls(self, sorted_by_version=True):
 
         results = super(PipNavigator, self).get_urls()
@@ -37,21 +39,9 @@ class PipNavigator(UrlNavigator):
             results = list(url_versions.keys())
 
         results = self._translate_pip_urls(results)
+
         return results
 
 
-class PipNavigatorFactory:
-
-    NAME = 'pip'
-
-    @CHECK_SUM_IMPL
-    def get_plugin_name(self):
-        return f'{self.NAME}'
-
-    @CHECK_SUM_IMPL
-    def create_navigator(self, name):
-        if name.lower() == self.NAME:
-            return PipNavigator
 
 
-pm.register(PipNavigatorFactory())
