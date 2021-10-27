1124abe58cac943a299ccf02ecc796369f583694
varioustoxins <garyt.and.sarahb@gmail.com>
2021-10-27 21:25:02 +0100

    corret posts process logi so it doesn't modify dictionary iterated on

--- tools/post_process_nmrpipe.py
+++ tools/post_process_nmrpipe.py
@@ -5,26 +5,25 @@ EXIT_ERROR = 1
 
 if __name__ == '__main__':
 
-    if (nargs := len(sys.argv))  != 2:
+    if (nargs := len(sys.argv)) != 2:
         print(f'I need one file to run on got {nargs-1}', file=sys.stderr)
         print('Exiting...')
         sys.exit(EXIT_ERROR)
 
     RESOURCE_REMOVALS = (
-        'NMRPipeL.tZ'
+        'NMRPipeL.tZ',
     )
 
 
     data = yaml.safe_load(open(sys.argv[1], 'rb'))
 
+    result = {}
     for version in data:
-
-        for elem in result[new_version]:
-
+        for elem, datum in data[version].items():
             if elem == 'resources':
-                for resource in result[new_version]['resources']:
-                    if resource in RESOURCE_REMOVALS and len(result[new_version]['resources'][resource]) < 3:
+                for resource in RESOURCE_REMOVALS:
+                    print(resource)
+                    if resource in data[version]['resources']:
                         del data[version]['resources'][resource]
 
     print(yaml.dump(data, default_flow_style=False, sort_keys=False))
-
