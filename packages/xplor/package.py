a610340cb6cbd89ce936451497d4ebfc792d7084
varioustoxins <garyt.and.sarahb@gmail.com>
2021-11-28 22:17:24 +0000

    initial xplor package supporting version 3.3

--- packages/xplor/package.py
+++ packages/xplor/package.py
@@ -33,16 +33,14 @@ if not package_root in sys.path:
 from nmrpack.lib.yaml_package import read_releases
 
 # this triggers
-import nmrpack.packages.cns.fetchers as fetchers
-
-from nmrpack.lib.environment import get_environment_change,PREPEND,NEW
+import nmrpack.lib.fetchers as fetchers
 
 import llnl.util.tty as tty
 import spack.error as error
 
-CNS_SOLVE_ENV = 'cns_solve_env'
+tar = which('tar')
+bash = which('bash')
 
-csh = which('csh')
 
 def check_config_file(*args,**kwargs):
 
@@ -53,8 +51,8 @@ def check_config_file(*args,**kwargs):
               'as an argument [configuration=<FILE_NAME>]'
         raise error.SpecError(msg)
     else:
-        result = fetchers.CNS_URL_Fetch_Strategy.check_configuration_file(value)
-        if result != fetchers.CNS_URL_Fetch_Strategy.OK:
+        result = fetchers.XPLOR_URL_Fetch_Strategy.check_configuration_file(value)
+        if result != fetchers.XPLOR_URL_Fetch_Strategy.OK:
             raise error.SpecError(f'Error with configuration {value} {result}')
 
 
@@ -62,150 +60,126 @@ def check_config_file(*args,**kwargs):
 class Xplor(Package):
     """XPLOR-NIH is a structure determination program which builds on the X-PLOR program, including additional tools developed at the NIH."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://nmr.cit.nih.gov/xplor-nih/"
-    url      = ""
+    homepage = "https://nmr.cit.nih.gov/xplor-nih"
 
-    # read_releases('xplor')
+    url='https://nmr.cit.nih.gov/xplor-nih/download.cgi?file=xplor-nih-3.3-db.tar.gz'
+    variant('configuration', default='none', description='where to find the configuration file', validator=check_config_file)
 
-    # # patch to make SETFPEPS (set floating point epsilon work)
-    # # see https://ask.bioexcel.eu/t/cns-errors-before-after-recompilation/54
-    # # main patch courtesy of Brian Smith U of Glasgow
-    # variant('configuration', default='none', description='where to find the configuration file', validator=check_config_file)
-    # variant('fp_epsilon', default=True, description='SETFPEPS required for modern compilers')
-    # variant('aria', default=True, description='patches required to run aria')
-    #
-    # resource(name='aria2.3.2', aria_url='http://aria.pasteur.fr/archives/aria2.3.2.tar.gz',
-    #          sha256='a06e9c256bf876f8bbab244a80b9c52bbd891f14cb956d85bb3bd0312b5cd5c6', destination='.')
-    #
-    # patch('getarch_darwin_x86_64.patch', when='@1.21',
-    #       sha256='42a0d10d7684000d5c4cf1114e14d5549cc19c84b19df4d42419abd7187cf887')
-    # patch('machvar_fpeps.patch', when='@1.21+fp_epsilon',
-    #       sha256='a00db99086c63961abe4e19d253590421973a80a9e104ac85dbcc07d472b6485')
-    #
-    # def install(self, spec, prefix):
-    #
-    #     # edit cns_solve_environment to allow a build
-    #     shutil.copy('cns_solve_env','cns_solve_env.back')
-    #     filter_file(r"setenv CNS_SOLVE '_CNSsolve_location_'", f"setenv CNS_SOLVE '{self.stage.source_path}'", 'cns_solve_env')
-    #
-    #     # copy over an almost right machine make file we could have got it from v1.3 but this is simpler
-    #     src_file = 'instlib/machine/supported/intel-x86_64bit-linux/Makefile.header.2.gfortran'
-    #     dest_file = 'instlib/machine/supported/mac-intel-darwin/Makefile.header.5.gfortran'
-    #     shutil.move(src_file, dest_file)
-    #
-    #
-    #     if not self.spec.satisfies('%fortran@:10.0.0'):
-    #         # patch the machine make file, can't be done with a patch statement it doesn't exists till we copy it
-    #         # tried just copying the file from the package directory but it caused a lockup
-    #         patch = which('patch')
-    #         patch_file = join_path(package_root,'nmrpack/packages/cns', 'gfortran_10_allow_argument_mismatch.patch')
-    #         patch('-p1', '-i',patch_file)
-    #
-    #     if '+aria' in self.spec:
-    #         from_path=pathlib.Path('aria2.3/cns/src')
-    #         to_path='source'
-    #
-    #         for target_file in from_path.iterdir():
-    #             if target_file.is_file() and target_file.suffix in ('.f','.inc'):
-    #                 print(f'copying {target_file} to {to_path}')
-    #                 shutil.copy(target_file,to_path)
-    #             if target_file.is_dir():
-    #                 print(f'copying {target_file} to {to_path}')
-    #                 shutil.copytree(target_file, join_path(to_path, target_file.name))
-    #
-    #         shutil.copytree(from_path,'aria2.3_patches_applied')
-    #         shutil.rmtree('aria2.3')
-    #
-    #     make('install')
-    #
-    #     install_tree('.',prefix)
-    #
-    #     with working_dir(prefix):
-    #         shutil.move('cns_solve_env.back', 'cns_solve_env')
-    #
-    #
-    #         replacement_env = f" setenv CNS_SOLVE  '{prefix}'"
-    #         filter_file(r"setenv CNS_SOLVE '_CNSsolve_location_'", replacement_env, 'cns_solve_env')
-    #
-    #     # remove a leftover from our previous edits
-    #     os.remove(pathlib.Path(prefix)  / pathlib.Path('cns_solve_env' + '~'))
-    #
-    # @run_after('install')
-    # @on_package_attributes(run_tests=True)
-    # def test(self):
-    #     try:
-    #         import tempfile
-    #         with tempfile.TemporaryDirectory() as tmp_dir_name:
-    #             tmp_dir_name = '/tmp'
-    #
-    #             test_inp = f'''
-    #             stop
-    #             '''
-    #
-    #             with open(join_path(tmp_dir_name,'test.inp'),'w') as fp:
-    #                 fp.write(test_inp)
-    #
-    #
-    #             test_csh = f'''
-    #             source  {self.prefix}/cns_solve_env
-    #
-    #             cns_solve < {tmp_dir_name}/test.inp >& {tmp_dir_name}/test_output.txt
-    #             '''
-    #
-    #             with open(join_path(tmp_dir_name,'test.csh'), 'w') as fp:
-    #                 fp.write(test_csh)
-    #
-    #         csh(join(tmp_dir_name,'test.csh'))
-    #
-    #
-    #         expected = '''
-    #         ============================================================
-    #         |                                                          |
-    #         |            Crystallography & NMR System (CNS)            |
-    #         |                         CNSsolve                         |
-    #         |                                                          |
-    #         ============================================================
-    #          Version: 1.2 at patch level 1
-    #          Status: General release with ARIA enhancements
-    #         ============================================================
-    #         '''.split("\n")
-    #         expected = [line.strip() for line in expected if len(line)]
-    #
-    #         ok = True
-    #         result= ''
-    #         with open(f"{tmp_dir_name}/test_output.txt", 'r') as file_handle:
-    #             result = file_handle.readlines()
-    #             result = [line.strip() for line in result if len(line)]
-    #             for line in expected:
-    #                 if not line in result:
-    #                     tty.error(f'line --{line}-- not in result')
-    #                     ok = False
-    #                     break
-    #         if not ok:
-    #             tty.error(f'''during testing strings
-    #                           {expected}
-    #                           not found in test output")
-    #                        ''')
-    #             tty.error("")
-    #             tty.error(f" output was")
-    #             tty.error("")
-    #             for line in result:
-    #                 tty.error(line.strip())
-    #     except Exception as e:
-    #         tty.error('there was an error',e)
-    #
-    #
-    # def setup_run_environment(self, env):
-    #
-    #     environment_changes = get_environment_change(self.prefix, CNS_SOLVE_ENV)
-    #
-    #     # print('** environment_changes **', environment_changes)
-    #     #
-    #     for name,type, value in environment_changes:
-    #         if type == PREPEND:
-    #             env.prepend_path(name, value)
-    #         elif type == NEW:
-    #             env.set(name, value)
-    #         else:
-    #             raise Exception(f'unexpected change type {type}')
+
+    read_releases('xplor')
+
+    def installable_resource_filenames(self, spec):
+        resource_filenames = set(self.get_resource_names(spec))
+
+        for variant_name in self.variants:
+            if not f'+{variant_name}' in self.spec:
+                if hasattr(self, 'variant_files'):
+                    for variant_file in self.variant_files[variant_name]:
+                        resource_filenames.remove(variant_file)
+        return resource_filenames
+
+    def get_resource_names(self, spec):
+        result = []
+        for resource_spec, resources in self.resources.items():
+            if spec.satisfies(resource_spec):
+                result = [resource.name for resource in resources]
+                break
+
+        return result
+
+    def install(self, spec, prefix):
+
+        resource_filenames = self.installable_resource_filenames(spec)
+        all_filenames = [pathlib.Path(self.stage.archive_file).name, *resource_filenames]
+
+        for file_name in resource_filenames:
+            shutil.move(f'tmp_{file_name}/{file_name}', '.')
+
+        items = [f for f in os.listdir('.')]
+        for item in items:
+            if not item.startswith('tmp_') or item in all_filenames:
+                shutil.move(item, prefix)
+
+        os.chdir(prefix)
+
+        for file_name in all_filenames:
+            if file_name.endswith('tar.gz'):
+                tar('--strip-components', '1', '-zxvf', file_name)
+                os.remove(file_name)
+
+        bash('./configure')
+
+    @run_after('install')
+    @on_package_attributes(run_tests=True)
+    def test(self):
+
+        test_inp = '''
+        stop
+        '''
+
+        test_sh = '''
+        bin/xplor < %(tmp_dir_name)s/test.inp >& %(tmp_dir_name)s/test_output.txt
+        '''
+
+        expected = ['http://nmr.cit.nih.gov/xplor-nih        based on X-PLOR 3.851 by A.T. Brunger']
+
+        self.run_test(test_inp, test_sh, expected, name='xplor')
+
+        test_inp = '''
+        print('hello world')
+        '''
+        test_inp =  test_inp.strip()
+
+        test_sh = '''
+        bin/pyXplor %(tmp_dir_name)s/test.inp >& %(tmp_dir_name)s/test_output.txt
+        '''
+
+        expected = ['hello world']
+
+        self.run_test(test_inp, test_sh, expected, name="pyXplor")
+
+    def run_test(self, test_inp, test_sh, expected, name):
+
+        import tempfile
+        with tempfile.TemporaryDirectory() as tmp_dir_name:
+            tmp_dir_name = '/tmp'
+
+            with open(join_path(tmp_dir_name, 'test.inp'), 'w') as fp:
+                fp.write(test_inp)
+
+            test_sh = test_sh % {'tmp_dir_name': tmp_dir_name}
+
+            with open(join_path(tmp_dir_name, 'test.sh'), 'w') as fp:
+                fp.write(test_sh)
+
+        Exception(test_sh)
+        bash(join(tmp_dir_name, 'test.sh'))
+
+        ok = True
+
+        with open(f"{tmp_dir_name}/test_output.txt", 'r') as file_handle:
+            result = file_handle.readlines()
+            result = [line.strip() for line in result if len(line)]
+            for line in expected:
+                if not line in result:
+                    tty.error(f'line --{line}-- not in result')
+                    ok = False
+                    break
+        if ok:
+            tty.msg(f'test {name} passed!')
+        else:
+            tty.error(f'''during testing strings
+                          {expected}
+                          not found in test output")
+                       ''')
+            tty.error("")
+            tty.error(f" output was")
+            tty.error("")
+            for line in result:
+                tty.error(line.strip())
+
+    def setup_run_environment(self, env):
+
+        bin_path = pathlib.Path(self.prefix) / 'bin'
+        env.prepend_path('PATH', str(bin_path.resolve()))
+
