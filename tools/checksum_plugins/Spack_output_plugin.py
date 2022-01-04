a74b7e3b239c5d18ce4fa985a17f21da84a84e71
varioustoxins <garyt.and.sarahb@gmail.com>
2022-01-04 21:59:00 +0000

    update spack output plugin for latest data structures

--- tools/checksum_plugins/Spack_output_plugin.py
+++ tools/checksum_plugins/Spack_output_plugin.py
@@ -1,23 +1,36 @@
 import re
 
 # noinspection PyUnresolvedReferences
+import urllib
+from urllib.parse import urlparse
+
 from checksum_url import Navigator, transfer_page, OutputBase
 # noinspection PyUnresolvedReferences
 from plugins import register_output
 
 from jinja2 import Template
 
-output_template = '''
+from itertools import product
 
+# Supported archive extensions from spack @0.17.
+PRE_EXTS   = ["tar", "TAR"]
+EXTS       = ["gz", "bz2", "xz", "Z"]
+NOTAR_EXTS = ["zip", "tgz", "tbz", "tbz2", "txz"]
+
+# Add PRE_EXTS and EXTS last so that .tar.gz is matched *before* .tar or .gz
+ALLOWED_ARCHIVE_TYPES = [".".join(ext) for ext in product(
+    PRE_EXTS, EXTS)] + PRE_EXTS + EXTS + NOTAR_EXTS
+
+output_template = '''
 
 class {{name}}(PythonPackage):
     """{{info|wordwrap(78)}}."""
 
-    pypi     = "{{pypi}}"
+    url = "{{url}}"
     
     {% for item in versions -%}
-    version('{{item.version}}', {{item.digest_type}}='{{item.digest}}, 
-             url={{item.url}}, expand={{item.expand}}')
+    version('{{item.version}}', {{item.digest_type}}='{{item.digest}}', 
+             url='{{item.url}}'{{item.expand}})
     {% endfor%}
     
     {% if format == 'sdist'  %}
@@ -35,37 +48,34 @@ class SpackOutput(OutputBase):
 
     def __init__(self, target_args=None):
         super(SpackOutput, self).__init__(target_args=target_args)
-        self._info = []
+        self._info = {}
+        self._hash_type = None
 
-    def display_hash(self, target_url, _hash, url_field_length, index, num_urls=None):
+    def display_hash(self, target_url, _hash, url_field_length, index, num_urls=None, hash_type=None):
 
-        self._info.append((target_url, _hash, url_field_length, index, num_urls))
+        self._hash_type = hash_type
+        self._info[target_url] = _hash
 
     def _ordered_urls(self):
         for elem in self._info:
-            yield elem[0]
+            yield elem
 
     def _check_digests_agree(self, url, extra_version_info, i):
         pip_digest = extra_version_info[url]['digests'][self.digest]
-        download_digest = self._info[i][1]
+        download_digest = self._info[url]
         if not pip_digest == download_digest:
             msg = f"for url {url} {self.digest} from pip [{pip_digest}] and download [{download_digest}] don't agree"
             raise Exception(msg)
 
-    def _get_preferred_package_type(self, extra_version_info):
-        latest_url = list(self._ordered_urls())[0]
-        return extra_version_info[latest_url]['format']
-
     def _should_expand(self, url):
-        expand = '.tgz', '.tar.gz', '.tz', '.zip'
 
-        result = False
-        for extension in expand:
-            if url[:-len(extension)] ==  extension:
-                result = True
+        expand = False
+        for extension in ALLOWED_ARCHIVE_TYPES:
+            if url[-len(extension):] ==  extension:
+                expand = True
                 break
 
-        return result
+        return "" if expand else ", expand=False"
 
     def _build_version_data(self, extra_version_info):
         version_data = []
@@ -75,7 +85,8 @@ class SpackOutput(OutputBase):
 
             self._check_digests_agree(url, extra_version_info, i)
 
-            if version_info['type'] == 'main_file':
+            file_type = version_info['type'] if 'type' in version_info else 'main_file'
+            if file_type == 'main_file':
                 data = {
                     'version': version_info['version'],
                     'digest_type': self.digest,
@@ -86,42 +97,30 @@ class SpackOutput(OutputBase):
                 version_data.append(data)
         return version_data
 
-    @staticmethod
-    def _get_pypi_url_path(url):
-        url_segments = url.split('/')
-        result = '/'.join(url_segments[-2:])
+    def _is_pypi_package(self):
 
-        return result
-
-    @staticmethod
-    def _filter_versions_by_package_type(version_data, extra_version_info, preferred_package_type):
-        result = []
-        for version_datum in version_data:
-            package_format = extra_version_info[version_datum['url']]['format']
-            if package_format == preferred_package_type:
-                result.append(version_datum)
-
+        result = False
+        for url in self._info:
+            if urlparse(url).netloc =='pypi.io':
+                result = True
+                break
         return result
 
     def finish(self, extra_package_info, extra_version_info):
 
-        preferred_package_type = self._get_preferred_package_type(extra_version_info)
-
-        pattern = re.compile(r'(?<!^)(?=[A-Z])')
-        snake_name = pattern.sub('-', extra_package_info['name']).lower()
-        print(f'# package-name: py-{snake_name}')
-
-        latest_version_url = self._info[0][0]
-
         version_data = self._build_version_data(extra_version_info)
-        version_data = self._filter_versions_by_package_type(version_data, extra_version_info, preferred_package_type)
 
+        extra_package_info = extra_package_info if extra_package_info else {}
         extra_info = {
             **extra_package_info,
-            'pypi': self._get_pypi_url_path(latest_version_url),
+            'url': extra_package_info['website'],
             'versions': version_data,
-            'format':  preferred_package_type
         }
+
+        if self._is_pypi_package():
+            extra_info['name'] =  f"Py{extra_info['name']}"
+        if 'info' not in extra_info:
+            extra_info['info'] = 'FIXME: Put a proper description of your package here (no info found)'
 
         template = Template(output_template)
         print(template.render(extra_info))
