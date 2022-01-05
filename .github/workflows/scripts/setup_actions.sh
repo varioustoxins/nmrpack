dfde76a7e1c6f14676945578fc08ddd83b277a57
varioustoxins <garyt.and.sarahb@gmail.com>
2022-01-05 21:59:32 +0000

    try a different way to move all files to unused but not unused itself again!

--- .github/workflows/scripts/setup_actions.sh
+++ .github/workflows/scripts/setup_actions.sh
@@ -1,5 +1,5 @@
 mkdir unused
 # https://askubuntu.com/questions/91740/how-to-move-all-files-in-current-folder-to-subfolder second solution
-ls | grep -v unused | xargs mv -t unused
+ls | grep -v unused | xargs mv --target-directory unused
 mv unused/.github .
 rm -rf unused
