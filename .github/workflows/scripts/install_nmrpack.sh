42d14fa76302867f5a3a732a75de319959c5668d
varioustoxins <garyt.and.sarahb@gmail.com>
2022-01-05 22:13:35 +0000

    correct path to install_spack

--- .github/workflows/scripts/install_nmrpack.sh
+++ .github/workflows/scripts/install_nmrpack.sh
@@ -5,7 +5,7 @@ curl -L https://github.com/spack/spack/releases/download/v${SPACK_VERSION}/spack
 mkdir spack
 tar --strip-components=1  -C spack -zxvf spack.tar.gz
 
-. .github/workflows/install_spack.sh
+. .github/workflows/scripts/install_spack.sh
 
 curl -L https://github.com/varioustoxins/nmrpack/archive/master.zip -o nmrpack-master.zip
 unzip nmrpack-master.zip
