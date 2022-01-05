5015953de2f65144a173a59fd879d32b351255c3
varioustoxins <garyt.and.sarahb@gmail.com>
2022-01-05 21:17:04 +0000

    try modularized test in a differnt way (suing bash script)

---
+++ .github/workflows/install_nmrpack.sh
@@ -0,0 +1,16 @@
+
+SPACK_VERSION=$(<spack_version.txt)
+
+curl -L https://github.com/spack/spack/releases/download/v${SPACK_VERSION}/spack-${SPACK_VERSION}.tar.gz -o spack.tar.gz
+mkdir spack
+tar --strip-components=1  -C spack -zxvf spack.tar.gz
+
+. .github/workflows/install_spack.sh
+
+curl -L https://github.com/varioustoxins/nmrpack/archive/master.zip -o nmrpack-master.zip
+unzip nmrpack-master.zip
+mv nmrpack-master nmrpack
+. spack/share/spack/setup-env.sh
+spack repo add nmrpack
+
+
