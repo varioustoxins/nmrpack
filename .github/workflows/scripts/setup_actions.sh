2bdce048cc91e2935775b043d16e2de172b6e092
varioustoxins <garyt.and.sarahb@gmail.com>
2022-01-05 22:05:04 +0000

    exclude . and ..

--- .github/workflows/scripts/setup_actions.sh
+++ .github/workflows/scripts/setup_actions.sh
@@ -1,5 +1,4 @@
 mkdir unused
-# https://askubuntu.com/questions/91740/how-to-move-all-files-in-current-folder-to-subfolder second solution
-mv `ls -a | grep -v unused` unused
+mv `ls -A | grep -v unused` unused
 mv unused/.github .
 rm -rf unused
