9d84453983b3333404eee9195b3df9abbeaf7726
varioustoxins <garyt.and.sarahb@gmail.com>
2022-01-05 21:57:42 +0000

    try a different way to move all files to unused but not unused itself (triggers error)

--- .github/workflows/scripts/setup_actions.sh
+++ .github/workflows/scripts/setup_actions.sh
@@ -1,4 +1,5 @@
 mkdir unused
-mv !(unused) unused
+# https://askubuntu.com/questions/91740/how-to-move-all-files-in-current-folder-to-subfolder second solution
+ls | grep -v unused | xargs mv -t unused
 mv unused/.github .
 rm -rf unused
