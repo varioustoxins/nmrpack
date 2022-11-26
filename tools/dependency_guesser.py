c29a54aab72d26cc3b75ce1ceffee2aff13cece9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 00:04:54 +0000

    Adding tarfile member sanitization to extractall()

--- tools/dependency_guesser.py
+++ tools/dependency_guesser.py
@@ -73,7 +73,26 @@ def extract(directory, file_name):
             if extension == 'tar.gz':
                 handled = True
                 with tarfile.open(full_file_name, 'r:gz') as tar_file:
-                    tar_file.extractall(directory)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar_file, directory)
             elif extension in ('zip', 'whl'):
                 handled = True
                 with zipfile.ZipFile(full_file_name, 'r') as zip_file:
